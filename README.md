45dd5bf19dd143a3c2f8cdb498a20edf3cc89621
ayban <ayanbansal4@gmail.com>
2026-07-25 18:01:25 +0000

    added a few commands to the shell, including a clear command and a previous command that allows you to view your command history. The previous command is still a work in progress.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Oganesson
 A custom CLI built to replace my earlier bash wrapper, cshift-cli.
+
+This project is a work in progress, more info will come soon.
+
+You will have to build the program yourself as this targets any OS based on POSIX and some of them (looking at you MacOS) have different executable formats.
--- cmds.h
+++ cmds.h
@@ -2,7 +2,12 @@
 #include <fstream>
 #include <filesystem>
 #include <cstdlib>
-#include <boost>
+#include <boost/filesystem.hpp>
+#include <boost/process.hpp>
+#include <sys/types.h>
+#include <unistd.h>
+#include <pwd.h>
+
 
 void init() {
     std::filesystem::current_path(std::getenv("HOME"));
@@ -16,8 +21,43 @@ void notBuiltIn(std::string cmd) {
     boost::filesystem path = boost::process::search_path(cmd);
 
     if (!path.empty()) {
-        std::system("$PATH/" + cmd);
+        int result = std::system("$PATH/" + cmd);
+        if (result != 0) {
+            std::cout << "ERR: Failed to run command.\n";
+        }
     } else {
         std::cout << "ERR: Command not found.\n";
     }
+}
+
+void addToHist(std::string cmd) {
+    std::fstream history(std::getenv("HOME").concat("/history.oganesson"), std::ios::app);
+
+    if (!history.is_open()) {
+        std::cout << "ERR: Couldn't open history file.\n";
+        return;
+    }
+
+    history << "\n" << cmd;
+}
+
+void previous() {
+    char buffer;
+    std::string username;
+    uid_t uid = getuid();
+
+    struct passwd* pw = getpwuid(uid);
+
+    if (pw) {
+        username = pw->pw_name;
+    } else {
+        std::cout << "ERR: Couldn't find a username for you";
+        return;
+    }
+
+    std::fstream history(std::getenv("HOME").concat("/history.oganesson"), std::ios::ate);
+}
+
+void clear() {
+    std::cout << "\033[2J\033[H";
 }

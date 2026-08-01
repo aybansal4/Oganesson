914b655fc56223d9f55bc53d73f1b58f10358c28
ayban <ayanbansal4@gmail.com>
2026-08-01 17:45:30 +0000

    Added a small program that is in progress that will put the user into the actual interface

--- main.cpp
+++ main.cpp
@@ -1,2 +1,14 @@
 #include "cmds.h"
 
+int main() {
+    init();
+    std::fstream setup("setup.oganesson");
+    std::filesystem::path p = std::filesystem::current_path();
+
+    
+    std::string command;
+    std::string PROMPT;
+    while (true) {
+        std::cout << std::getline();
+    }
+}

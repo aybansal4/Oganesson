5c846b2824d28744b663501b0bff6ad6119a4733
ayban <ayanbansal4@gmail.com>
2026-08-01 17:48:22 +0000

    added a few commands to cmds.h

--- cmds.h
+++ cmds.h
@@ -7,57 +7,63 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <pwd.h>
+#include <cstdlib>
 
+namespace commands {
+    void init() {
+        std::filesystem::current_path(std::getenv("HOME"));
 
-void init() {
-    std::filesystem::current_path(std::getenv("HOME"));
+        std::fstream aliases("aliases.oganesson"); aliases.close();
+        std::fstream history("history.oganesson"); history.close();
+        std::fstream setup("setup.oganesson"); setup.close();
+    }
 
-    std::fstream aliases("aliases.oganesson"); aliases.close();
-    std::fstream history("history.oganesson"); history.close();
-    std::fstream setup("setup.oganesson"); setup.close();
-}
+    void notBuiltIn(std::string cmd) {
+        boost::filesystem path = boost::process::search_path(cmd);
 
-void notBuiltIn(std::string cmd) {
-    boost::filesystem path = boost::process::search_path(cmd);
-
-    if (!path.empty()) {
-        int result = std::system("$PATH/" + cmd);
-        if (result != 0) {
-            std::cout << "ERR: Failed to run command.\n";
+        if (!path.empty()) {
+            int result = std::system(cmd.c_str());
+            if (result != 0) {
+                std::cout << "ERR: Failed to run command.\n";
+            }
+        } else {
+            std::cout << "ERR: Command not found.\n";
         }
-    } else {
-        std::cout << "ERR: Command not found.\n";
-    }
-}
-
-void addToHist(std::string cmd) {
-    std::fstream history(std::getenv("HOME").concat("/history.oganesson"), std::ios::app);
-
-    if (!history.is_open()) {
-        std::cout << "ERR: Couldn't open history file.\n";
-        return;
     }
 
-    history << "\n" << cmd;
-}
+    void addToHist(std::string cmd) {
+        std::fstream history(std::getenv("HOME").concat("/history.oganesson"), std::ios::app);
 
-void previous() {
-    char buffer;
-    std::string username;
-    uid_t uid = getuid();
+        if (!history.is_open()) {
+            std::cout << "ERR: Couldn't open history file.\n";
+            return;
+        }
 
-    struct passwd* pw = getpwuid(uid);
-
-    if (pw) {
-        username = pw->pw_name;
-    } else {
-        std::cout << "ERR: Couldn't find a username for you";
-        return;
+        history << "\n" << cmd;
     }
 
-    std::fstream history(std::getenv("HOME").concat("/history.oganesson"), std::ios::ate);
-}
+    void previous() {
+        char buffer;
+        std::string username;
+        uid_t uid = getuid();
 
-void clear() {
-    std::cout << "\033[2J\033[H";
+        struct passwd* pw = getpwuid(uid);
+
+        if (pw) {
+            username = pw->pw_name;
+        } else {
+            std::cout << "ERR: Couldn't find a username for you";
+            return;
+        }
+
+        std::fstream history(std::getenv("HOME").concat("/history.oganesson"), std::ios::ate);
+    }
+
+    void clear() {
+        std::cout << "\033[2J\033[H";
+    }
+
+    void chdir(std::string path) {
+        chdir(path);
+    }
 }

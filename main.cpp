95d462b9d35de244e0a8568ac161bc4c8ed712bf
Ayan Bansal <ayanbansal@Ayans-MacBook-Air.local>
2026-09-05 13:30:51 -0400

    add random stuff for testing

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <filesystem>
+#include <boost/algorithm/string.hpp>
+#include <vector>
 #include "cmds.h"
 
 int main() {
@@ -11,8 +13,12 @@ int main() {
     while (true) {
         std::cout << "┌ user: " << std::filesystem::current_path() << "\n└---> ";
         std::getline(std::cin, command);
-        std::cout << command << "\n";
+        
+        std::vector<std::string> cmd_split;
+        boost::split(cmd_split, command, boost::is_any_of(" "));
+        for (std::string w : cmd_split) std::cout << w << "\n";
         if (command == "exit") return 0;
+
 
     }
 }
